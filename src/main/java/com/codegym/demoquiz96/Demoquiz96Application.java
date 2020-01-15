db509e90942ba0b9324871ce4f85c536ea0ccad9
DinhHieu96 <truongdinhhieu1496@gamil.com>
2020-01-15 11:23:09 +0700

    project test

---
+++ src/main/java/com/codegym/demoquiz96/Demoquiz96Application.java
@@ -0,0 +1,13 @@
+package com.codegym.demoquiz96;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoquiz96Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demoquiz96Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/demoquiz96/Demoquiz96ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.demoquiz96;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoquiz96ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
