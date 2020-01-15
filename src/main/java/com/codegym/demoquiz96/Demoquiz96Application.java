888f5b56aa4b74e47df3816379f4fe5627385584
DinhHieu96 <truongdinhhieu1496@gamil.com>
2020-01-15 11:50:29 +0700

    application config

--- src/main/java/com/codegym/demoquiz96/Demoquiz96Application.java
+++ src/main/java/com/codegym/demoquiz96/Demoquiz96Application.java
@@ -2,8 +2,12 @@ package com.codegym.demoquiz96;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.codegym.demoquiz96.repository")
+@EntityScan("com.codegym.demoquiz96")
 public class Demoquiz96Application {
 
     public static void main(String[] args) {
